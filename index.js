018d6db909f3a20bf7c098379c6e25f6cd79793f
SAMUEL HAILE <samishken@gmail.com>
2018-02-16 12:38:14 -0500

    update

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
+#! /usr/bin/env node
 
 const fs = require('fs')
 
-newFunction()
-
-fs.readFile(__dirname + '/info.json', 'utf8', function (err, data) {
-  if (err) {
-    console.log(err)
-  } else {
-    console.log(data)
-    return data
-  }
+fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(data)
+        return data
+    }
 })
